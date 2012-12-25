07dba688e69891ff5fc6c59352e82dfdc5bc6150
drupalka <sm@eka-soft.com>
2012-12-26 00:09:01 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '5987ce2b6ec3538f83ec4876ea333dc6a523ed161719d118635b0ef6b599b7157082f7c40647d46abc3b4344bd14b95a2af8ff2415a4cdadf1920887c8870baf'
